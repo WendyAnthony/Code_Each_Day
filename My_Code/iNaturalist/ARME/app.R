177fa5a0f945b7c9f53ab5f1d7b9a6702a5c3c11
Wendy Anthony <WendyAnthony@users.noreply.github.com>
2021-06-03 17:34:39 -0700

    Add files via upload

--- My_Code/iNaturalist/ARME/app.R
+++ My_Code/iNaturalist/ARME/app.R
@@ -54,6 +54,14 @@ iNat_ARME_n_bounds_2 <- get_inat_obs(
   maxresults = 10000   #5292 actual
 )
 
+
+s_bounds_2 <- c(32.74801, -124.67834, 49.21558, -108.67676)
+iNat_ARME_s_bounds_2 <- get_inat_obs(
+  query = "Arbutus menziesii",
+  bounds = s_bounds_2,
+  maxresults = 10000 #2530 actual
+)
+
 s_bounds <- c(40.37166, -125.68359, 48.2082, -106.17187)
 iNat_ARME_s_bounds <- get_inat_obs(
   query = "Arbutus menziesii",
@@ -78,6 +86,16 @@ iNat_ARME_s_bounds <- get_inat_obs(
 # SW 40.37166, -121.99219
 # SE 41.69753, -106.17187
 
+
+# southern latitude, western longitude, northern latitude, and eastern longitude.
+# s_bounds_2
+# 32.74801, -124.67834, 49.21558, -108.67676
+#SW 32.74801, -124.7168
+#NW 48.1457, -124.67834
+#NE 49.21558, -120.6189
+#SE 34.61287, -108.67676
+
+
 #################
 # iNat_ARME_query_10000
 # iNat_ARME_n_bounds
@@ -95,14 +113,15 @@ maptypes <- c("Stamen.TerrainBackground",
 # shiny app
 ui <- fluidPage(
   titlePanel("ARME iNaturalist Observations Leaflet Map Viz"),
-  tags$br(),
+  #tags$br(),
   tags$p("Click marker clusters to find individual observations"),
-  tags$br(),
+  #tags$br(),
   
   mainPanel(
     tabsetPanel(
       tabPanel("ARME North", leafletOutput("mymap_n")),
-      tabPanel("ARME South", leafletOutput("mymap_s")),
+      tabPanel("ARME South (Wash)", leafletOutput("mymap_s")),
+      tabPanel("ARME South (Cal)", leafletOutput("mymap_s_2")),
       tabPanel("About", tableOutput("text"))
     )
   )
@@ -112,8 +131,8 @@ server <- function(input, output, session) {
   output$mymap_n <- renderLeaflet({
     # leaflet map with popup of many characters & image link
     map <- leaflet(height="3800px", width = "100%") %>%
-      setView(lng = -125.52,  # 49.54782, -125.5188
-              lat = 49.54,
+      setView(lng = -124.04,  # 52.60889, -124.03564  #52.63556, -126.60645
+              lat = 52.61,
               zoom = 5) %>% 
       addTiles(group = "OSM (default)",
                options = providerTileOptions(minZoom = 3, maxZoom = 25)) %>%
@@ -143,9 +162,9 @@ server <- function(input, output, session) {
   output$mymap_s <- renderLeaflet({
     # leaflet map with popup of many characters & image link
     map <- leaflet(height="3800px", width = "100%") %>%
-      setView(lng = -125.52,  # 49.54782, -125.5188
-              lat = 49.54,
-              zoom = 5) %>% 
+      setView(lng = -122.39,  # 44.98714, -122.3877
+              lat = 44.99,
+              zoom = 6) %>% 
       addTiles(group = "OSM (default)",
                options = providerTileOptions(minZoom = 3, maxZoom = 25)) %>%
       addProviderTiles(providers$Stamen.Toner, group = "Toner",
@@ -171,6 +190,36 @@ server <- function(input, output, session) {
       )
   })
   
+  output$mymap_s_2 <- renderLeaflet({
+    # leaflet map with popup of many characters & image link
+    map <- leaflet(height="3800px", width = "100%") %>%
+      setView(lng = -118.89,  # 39.73148, -118.89404
+              lat = 39.73,
+              zoom = 5) %>% 
+      addTiles(group = "OSM (default)",
+               options = providerTileOptions(minZoom = 3, maxZoom = 25)) %>%
+      addProviderTiles(providers$Stamen.Toner, group = "Toner",
+                       options = providerTileOptions(minZoom = 3, maxZoom = 25)) %>%
+      addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite",
+                       options = providerTileOptions(minZoom = 3, maxZoom = 25)) %>%
+      addProviderTiles(providers$Esri.NatGeoWorldMap, group = "Nat Geo",
+                       options = providerTileOptions(minZoom = 3, maxZoom = 25)) %>%
+      addProviderTiles(providers$Esri.WorldImagery, group = "ESRI World",
+                       options = providerTileOptions(minZoom = 3, maxZoom = 25)) %>%
+      
+      
+      #      addProviderTiles("Esri.WorldImagery") %>% 
+      #     addProviderTiles(maptypes[1]) %>% # chose other basemap by number
+      addMarkers(lat = iNat_ARME_s_bounds_2$latitude, 
+                 lng = iNat_ARME_s_bounds_2$longitude,
+                 clusterOptions = markerClusterOptions(spiderfyOnMaxZoom = T),
+                 #    clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F),
+                 popup = paste("<b>", "Scientific Name:", "</b>",  "<i>",  iNat_ARME_s_bounds_2$scientific_name, "</i>", "<br>", "<b>", "Common Name:", "</b>", iNat_ARME_s_bounds_2$common_name, "<br>", "<b>", "Place:", "</b>", iNat_ARME_s_bounds_2$place_guess, "<br>", "<b>", "iNaturalist Link:", "</b>", "<a href='", iNat_ARME_s_bounds_2$url, "<b>",  "'>Observation</a>", "<br>", "<img src='", iNat_ARME_s_bounds_2$image_url, "'width='200px' />", "<br>", "<b>", "Taxon:", "</b>", iNat_ARME_s_bounds_2$iconic_taxon_name, "<br>", "<b>", "Observation Date:", "</b>", iNat_ARME_s_bounds_2$observed_on_string, "<br>", "<b>", "Citizen Scientist / Photographer:", "</b>", iNat_ARME_s_bounds_2$user_login )) %>%
+      addLayersControl(
+        baseGroups = c("OSM (default)", "Toner", "Toner Lite", "Nat Geo", "ESRI World"),
+        options = layersControlOptions(collapsed = TRUE)
+      )
+  })
   output$text <- renderUI({
     str1 <- tags$h2("iNaturalist observation Visualization")
     str2 <- paste("iNaturalists observations of Arbutus menziesii")
@@ -178,11 +227,12 @@ server <- function(input, output, session) {
     str4 <- paste("These are observations collected using @ iNaturalist")
     str5 <- tags$a(href="https://github.com/WendyAnthony/Code_Each_Day/blob/master/My_Code/iNaturalist/ARME/app.R", "Shiny app Code")
     str6 <- tags$a(href="https://www.inaturalist.org/observations?locale=en&subview=map&taxon_id=51046", "iNaturalist Observations")
-    str7 <- paste("Created by Wendy Anthony 2021-06-03")
-    HTML(paste(str1, str2, str3, str4, str5, str6, str7, sep = "<br /><br />"))
+    str7 <- tags$a(href="https://www.arbutusarme.org/home", "Arubuts website home")
+    str8 <- tags$a(href="https://treesnap.org/map/?center=47.66351640199509,-121.52321502871439&zoom=8", "Treesnap Observations")
+    str9 <- paste("Created by Wendy Anthony 2021-06-03")
+    HTML(paste(str1, str2, str3, str4, str5, str6, str7, str8, str9, sep = "<br /><br />"))
   })
   # https://stackoverflow.com/questions/23233497/outputting-multiple-lines-of-text-with-rendertext-in-r-shiny  
 }
-
 
 shinyApp(ui, server)
