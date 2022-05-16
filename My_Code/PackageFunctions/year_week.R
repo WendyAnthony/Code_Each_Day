643275a4845917f86476da13b4bb2d099cc990bd
Wendy Anthony <WendyAnthony@users.noreply.github.com>
2022-05-15 20:44:46 -0700

    Add files via upload

---
+++ My_Code/PackageFunctions/year_week.R
@@ -0,0 +1,12 @@
+year_week <- function(yyyy, mm, dd){
+
+  str1 <- yyyy
+  str2 <- mm
+  str3 <- dd
+
+  date <- paste(str1, str2, str3, sep = "-")
+
+  date1 <- as.Date(date, format = "%Y-%m-%d")
+  # https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/strptime
+  strftime(date1, format = "%V")
+}
