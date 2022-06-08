326dcf8c3f874e6b8dc07c919473f962de85e8c3
Wendy Anthony <WendyAnthony@users.noreply.github.com>
2022-06-07 18:50:38 -0700

    Add files via upload

--- My_Code/PackageFunctions/natmapr/inat_leaflet_save.R
+++ My_Code/PackageFunctions/natmapr/inat_leaflet_save.R
@@ -3,5 +3,8 @@ inat_leaflet_save <- function(m, filename) {
 }
 
 
+# save in specific folder
+# inat_leaflet_save(m "./inst/extdata/filename.html")
 
-# inat_leaflet_save("esq_lag.html")
+# save in working directory folder
+# inat_leaflet_save(m "filename.html")
