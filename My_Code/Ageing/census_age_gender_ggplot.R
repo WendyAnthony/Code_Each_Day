1ee79a2c24725d88393792e552889836d03b3c48
Wendy Anthony <WendyAnthony@users.noreply.github.com>
2022-08-23 00:57:28 -0700

    Add files via upload

--- My_Code/Ageing/census_age_gender_ggplot.R
+++ My_Code/Ageing/census_age_gender_ggplot.R
@@ -1,22 +1,18 @@
-census_age_gender_ggplot <- function(data, colpat1, colpat2, title, subtitle, caption, labelsize, filename = NULL, aspect_ratio = 2.5){
+census_age_gender_ggplot <- function(data, colpat1, colpat2, title, subtitle, caption, labelsize, filename, aspect_ratio = 2.5){
 
   # clean data
   data[data == "Women+"] <- "Women"
   data[data == "Men+"] <- "Men"
   colnames(data) <- c("age_group", "gender", "pop")
 
-  # format pop with commas in thousands
-#  formatC(data$pop, format = "d", big.mark = ",")
-
-
   # sort age values
   data$age_group <- factor(data$age_group, levels = str_sort(unique(data$age_group), numeric = TRUE))
 
   # Colour brewer color-blind safe
-   colour_palette <- c(colpat1, colpat2)
+  colour_palette <- c(colpat1, colpat2)
 
-   # ggplot
-   ggplot(data, aes(x = age_group, y = pop, fill = gender)) +
+  # ggplot
+  ggplot(data, aes(x = age_group, y = pop, fill = gender)) +
     geom_bar(stat = "identity", position = "dodge", alpha = 0.95, width = 0.85)  +
     # wideth = 1 no space between bars
     geom_text(aes(label = pop), size = labelsize, position = position_dodge(width = 0.9), vjust = 0.35, hjust = 1.05, angle = 90) +
@@ -36,20 +32,20 @@ census_age_gender_ggplot <- function(data, colpat1, colpat2, title, subtitle, ca
          y = "Population",
          caption = caption)
 
-   aspect_ratio <- aspect_ratio
-   ggsave(filename, height = 10 , width = 4 * aspect_ratio)
+  aspect_ratio <- aspect_ratio
+  ggsave(filename, height = 10 , width = 4 * aspect_ratio)
 }
 
 
 
 
 p1 <- census_age_gender_ggplot(data = Victoria_pop_age_gender_2021_seniors,
-        colpat1 = "#af8dc3", colpat2 = "#7fbf7b",
-        title = "Victoria Seniors Population \n by Age & Gender",
-        subtitle = "(2021 Canada Census)",
-        caption = "Data: https://www12.statcan.gc.ca/census-recensement/2021",
-        labelsize = 3,
-        filename = "Victoria_pop_age_gender_2021_seniors.jpg"
+                               colpat1 = "#af8dc3", colpat2 = "#7fbf7b",
+                               title = "Victoria Seniors Population \n by Age & Gender",
+                               subtitle = "(2021 Canada Census)",
+                               caption = "Data: https://www12.statcan.gc.ca/census-recensement/2021",
+                               labelsize = 3,
+                               filename = "Victoria_pop_age_gender_2021_seniors.jpg"
 )
 p1
 
@@ -57,12 +53,12 @@ p1
 # BC_pop_2021$pop <- formatC(BC_pop_2021$pop, format = "d", big.mark = ",")
 
 p2 <- census_age_gender_ggplot(data = BC_pop_2021,
-         colpat1 = "#af8dc3", colpat2 = "#7fbf7b",
-         title = "BC Population \n by Age & Gender",
-         subtitle = "(2021 Canada Census)",
-         caption = "Data: https://www12.statcan.gc.ca/census-recensement/2021",
-         labelsize = 3,
-         filename = "BC_pop_2021.jpg"
+                               colpat1 = "#af8dc3", colpat2 = "#7fbf7b",
+                               title = "BC Population \n by Age & Gender",
+                               subtitle = "(2021 Canada Census)",
+                               caption = "Data: https://www12.statcan.gc.ca/census-recensement/2021",
+                               labelsize = 3,
+                               filename = "BC_pop_2021.jpg"
 )
 p2
 
@@ -75,6 +71,3 @@ p3 <- census_age_gender_ggplot(data = BC_pop_2021_seniors,
                                filename = "BC_pop_2021_seniors.jpg"
 )
 p3
-
-
-
