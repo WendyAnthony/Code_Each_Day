1dcd5bfa8025c1f5d0baccf3762dd10f684ac813
Wendy Anthony <WendyAnthony@users.noreply.github.com>
2023-02-02 10:09:18 -0800

    Add files via upload

--- My_Code/GeogInteractive/Shiny Gantt Chart Interactive/ShinyMermaidFlowChartInteractive/app.R
+++ My_Code/GeogInteractive/Shiny Gantt Chart Interactive/ShinyMermaidFlowChartInteractive/app.R
@@ -1,6 +1,6 @@
 ## -----------------------------------------
 ## -----------------------------------------
-## Shiny App File created by Wendy Anthony
+## Mermaid Shiny App File created by Wendy Anthony
 # 2023-02-01 10:51
 ## for Interactive UVic Geography Course Planning
 ## app inspiration from:
@@ -8,12 +8,12 @@
 ## -----------------------------------------
 ## -----------------------------------------
 
+## -----------------------------------------
 ## Load Shiny Library ----------------------
 # uncomment the install.packages code to
 # install packeage, then re-comment to avoid
 # intalling packages each time the app is run
 # calling the library after installation is enough
-#
 #
 # install.packages("shiny")
 # install.packages("DiagrammeR")
@@ -22,7 +22,7 @@ library(DiagrammeR)
 
 ## -----------------------------------------
 ## Define UI for app -----------------------
-ui <- fluidPage(
+ui <- fluidPage (
 
   ## ---------------------------------------
   ## Application title -------------------
@@ -31,28 +31,6 @@ ui <- fluidPage(
   tags$head(tags$style(".button{background-color:#69A81D;} .button{color: #f0f6e8;} .button{margin: auto;}
                        .button-center{text-align: left;}")),
 
-  ## ------------------------------------------
-    ## Sidebar with some input ----------------
-    sidebarLayout(
-
-      ## --------------------------------------
-      ## Begin sidebarPanel -------------------
-        sidebarPanel(
-
-          ## Begin Intro Note --------------------------------------
-          #tags$b("How to use this interactive Gantt Chart"),
-
-          ## End Intro Note --------------------------------------
-
-            # # Begin someInput ---------------
-
-          tableOutput("instruct"),
-
-
-            # # End someInput -----------------
-
-        ), ## End sidebarPanel ----------------
-        ## ------------------------------------
 
         ## ------------------------------------
         ## Begin mainPanel --------------------
@@ -63,14 +41,14 @@ ui <- fluidPage(
 
 
           ## -------------------------------------------------------
-          # Begin Gantt --------------------------------------------
+          # Begin Mermaid --------------------------------------------
 
-          ## Begin Gantt tableOutput -----------------------------------
+          ## Begin Mermaid tableOutput -----------------------------------
           ## -------------------------------------------------------
           # https://stackoverflow.com/questions/57351127/renderdiagrammer-mermaid-diagram-size-in-a-shiny-document
           tableOutput("mermaid"),
 
-                   ## Begin Gantt textInput -----------------------------------
+                   ## Begin Mermaid textInput -----------------------------------
                    ## -------------------------------------------------------
                    textAreaInput(inputId = "inText", label = NULL, width = "900px", height = "370px", rows = 15, value = "
 graph LR;
@@ -84,12 +62,23 @@ E5[GEOG 226]
 E6[GEOG 230]
 E7[GEOG 252]
 E8[GEOG 304]
-E9[GEOG 353]
-E10[GEOG 438]
+E8a[GEOG 319]
+E8b[GEOG 322]
+E8c[GEOG 323]
+E9a[GEOG 353]
+E9b[GEOG 358]
+E10a[GEOG 422]
+E10b[GEOG 438]
 E11[GEOG 453]
 
 B-->A;
 E1-->E5;
+E5-->E8a;
+E5-->E8b;
+E5-->E9b;
+E5-->E8c;
+E8a-->E10a;
+E8b-->E10a;
 E3-->E5;
 E4-->E6;
 E7-->E8;
@@ -97,13 +86,12 @@ E7-->E9;
 E9-->E11;
 
                                   "),
-                   ## End Gantt textInput -----------------------------------
+                   ## End Mermaid textInput -----------------------------------
                    ## -------------------------------------------------------
 
                   ## -------------------------------------------------------
                   ## Begin downloadButton --------------------------------------
-                  div(class = "button-center", downloadButton("download", "Download textInput.txt", class="button")),
-#                  div(class = "button-center", downloadButton("save", "Save image", class="button")),
+                    div(class = "button-center", downloadButton("download", "Download textInput.txt", class="button")),
                   ## -------------------------------------------------------
                   ## End downloadButton --------------------------------------
 
@@ -111,21 +99,22 @@ E9-->E11;
                    ## Begin DiagrammeR --------------------------------------
                    DiagrammeR::DiagrammeROutput(outputId = "diagram", width = "950px", height = "auto"),
                    hr(),
-                   h6("Shiny code by Wendy Anthony <wanthony@uvic.ca> 2023-02-01",
-                      align="left", style = "font-family: sans-serif; font-weight: 1px; font-size: 10px;
-text-shadow: 0px 0px 1px #aaa; line-height: 1; color: #404040;"),
-
+                   h6("Shiny code by Wendy Anthony <wanthony@uvic.ca> 2023-02-02",
+                      align="left", style = "font-family: sans-serif; font-weight: 1px; font-size: 10px; 
+                      text-shadow: 0px 0px 1px #aaa; line-height: 1; color: #404040;"),
                    ## End DiagrammeR ----------------------------------------
                    ## -------------------------------------------------------
 
-          # end of Gantt  ---------------------------------------
+                   # # Begin someInput ---------------
+                  tableOutput("instruct"),
+                  # # End someInput -----------------
+
+          
+          # end of Mermaid  ---------------------------------------
 
           ), # end of mainPanel  ---------------------------------------
           ## -------------------------------------------------------
 
-
-        ), ## End  of sidebarLayout -------------------
-        ## -----------------------------------
 
   tags$img(src = "https://people.geog.uvic.ca/wanthony/website/geog-curriculum-maps/images/Dynamic-edge-transparent-1.png", height = 50, width = "100%")
 
@@ -143,17 +132,6 @@ server <- function(input, output) {
   ## ----------------------------------------------
   ## Begin Download Button ----------------------
   ## ----------------------------------------------
-  # output$download_filtered <- downloadHandler(
-  #   filename = function() {
-  #     paste("GeographyCourses-FilteredData-", Sys.Date(), ".csv", sep="")
-  #   },
-  #   content = function(file) {
-  #     write.csv(geog_dt[input[["dt_rows_all"]], ],
-  #               file= file,
-  #               row.names=F)
-  #   }
-  # ) # end of Output: Datatable downloadHandler
-
   output$download <- downloadHandler(
     filename = function() {
       paste("mermaid-flowchart-TextInput-", Sys.Date(), ".txt", sep="")
@@ -162,7 +140,6 @@ server <- function(input, output) {
       cat(input$inText, file=file)
     }
   )
-
   ## ----------------------------------------------
   ## End Download Button ----------------------
   ## ----------------------------------------------
@@ -171,38 +148,29 @@ server <- function(input, output) {
   ## ----------------------------------------------
   ## Begin output$someOutput ----------------------
   ## ----------------------------------------------
-        #   # Output: Gantt2    diagramme
+        #   # Output: Mermaid    diagramme
         output$diagram <-
           DiagrammeR::renderDiagrammeR({
             DiagrammeR::mermaid(
               base::paste0(input$inText)
             )
-          }) # end of Output: Gantt2    diagramme
+          }) # end of Output: Mermaid    diagramme
   ## ----------------------------------------------
   ## End output$someOutput ------------------------
   ## ----------------------------------------------
-        # output$save <- downloadHandler(
-        #   filename = function() {
-        #     paste(input$diagram, '.png', sep='') },
-        #   content = function(file) {
-        #     ggsave(file, plotInput())
-        #   }
-        # )
-
-
 
       ## ------------------------------------------
       ## Begin output$someOutput ------------------
       ## ------------------------------------------
           ## --------------------------------------
-          #   # Output: Gantt
+          #   # Output: Mermaid
           output$mermaid <- renderUI({
             HTML("
                    <span><strong>To update the Mermaid Flow Chart in real-time:</strong>
                    <br>* Add letters that correspond to coursel; use arrow keys to show relationships
                    <br>* Download textInput results as mermaid-TextInput.txt file
                    <br><br>")
-          }) # end of Output: Gantt
+          }) # end of Output: Mermaid
           ## --------------------------------------
       ## ------------------------------------------
       ## End output$someOutput --------------------
@@ -248,9 +216,6 @@ server <- function(input, output) {
           ## End output$someOutput -------------------
           ## -----------------------------------------
           ## -----------------------------------------
-
-
-
 
 
 
